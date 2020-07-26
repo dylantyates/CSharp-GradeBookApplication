6ae5b07dfaa0d846d262ec888fd14fa438e990d6
dylantyates <dylan_yates1@homedepot.com>
2020-07-26 07:29:47 -0500

    feat: module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -25,10 +25,30 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(s => s.AverageGrade)
                                  .Select(s => s.AverageGrade)
                                  .ToList();
+
             return StudentsRankedByGrade(averageGrade, threshold, grades);
         }
 
-        private static char StudentsRankedByGrade(double averageGrade, int threshold, List<double> grades)
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStudentStatistics(name);
+        }
+
+        private char StudentsRankedByGrade(double averageGrade, int threshold, List<double> grades)
         {
             // Highest 20% of students get A, next 20% get B, so on...
             if (averageGrade >= grades[threshold - 1])
